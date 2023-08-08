a2019287c285e8f811ca264c4f5b9e5bff82a47c
Pedro Teodoro <pedropauloteodoro26@gmail.com>
2023-08-08 10:33:38 -0300

    adding and updating functions and variables

--- js/sounds.js
+++ js/sounds.js
@@ -1,9 +1,21 @@
+import { elements } from "./elements.js"
+
+const {
+  cardForest, cardRain, cardCoffeeShop, cardFireplace, inputForestVolume, inputRainVolume, inputCoffeeShopVolume, inputFireplaceVolume
+} = elements
+
 export default function() {
   const buttonPressAudio = new Audio("./assets/button-press.wav")
   const kitchenTimer = new Audio("./assets/kichen-timer.mp3")
-  const bgAudio = new Audio("./assets/bg-audio.mp3")
+  const bgAudioForest = new Audio("./assets/Floresta.wav")
+  const bgAudioRain = new Audio("./assets/Chuva.wav")
+  const bgAudioCoffeeShop = new Audio("./assets/Cafeteria.wav")
+  const bgAudioFireplace = new Audio("./assets/Lareira.wav") 
 
-  bgAudio.loop = true
+  bgAudioForest.loop = true
+  bgAudioRain.loop = true
+  bgAudioCoffeeShop.loop = true
+  bgAudioFireplace.loop = true
 
   function pressButton() {
     buttonPressAudio.play()
@@ -13,7 +25,54 @@ export default function() {
     kitchenTimer.play()
   }
 
-  
+  function audioForest() {
+    cardForest.classList.contains('active')
+    ? bgAudioForest.play()
+    : bgAudioForest.pause()
+  }
 
-  return {pressButton, timeEnd, bgAudio}
+  function audioRain() {
+      cardRain.classList.contains('active')
+      ? bgAudioRain.play()
+      : bgAudioRain.pause()
+  }
+
+  function audioCoffeeShop() {
+      cardCoffeeShop.classList.contains('active')
+      ? bgAudioCoffeeShop.play()
+      : bgAudioCoffeeShop.pause()
+  }
+
+  function audioFireplace() {
+      cardFireplace.classList.contains('active')
+      ? bgAudioFireplace.play()
+      : bgAudioFireplace.pause()
+  }
+
+  function setAudioVolume() {
+      bgAudioForest.volume = inputForestVolume.value
+      bgAudioRain.volume = inputRainVolume.value
+      bgAudioCoffeeShop.volume = inputCoffeeShopVolume.value
+      bgAudioFireplace.volume = inputFireplaceVolume.value
+  }
+
+  function resetForestVolume() {
+      inputForestVolume.value = 0.5
+  }
+
+  function resetRainVolume() {
+      inputRainVolume.value = 0.5
+  }
+
+  function resetCoffeeShopVolume() {
+      inputCoffeeShopVolume.value = 0.5
+  }
+
+  function resetFireplaceVolume() {
+      inputFireplaceVolume.value = 0.5
+  }
+
+    
+
+    return { pressButton, timeEnd, audioForest, audioRain, audioCoffeeShop, audioFireplace, setAudioVolume, resetForestVolume, resetRainVolume, resetCoffeeShopVolume, resetFireplaceVolume }
 }
